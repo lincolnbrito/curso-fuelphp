fab14b48f562df04e05e1dabcad8173e89eef899
Lincoln Brito <lincoln.sbrito@gmail.com>
2014-10-10 09:39:45 -0300

    criando controller categorias

---
+++ fuel/app/classes/controller/categorias.php
@@ -0,0 +1,13 @@
+<?php
+class Controller_Categorias extends Controller
+{
+	public function action_index()
+	{
+		echo 'oi, eu sou a index do controller categorias';
+	}
+
+	public function action_listar()
+	{
+		echo 'oi, eu sou o método listar do controller categorias';
+	}
+}
